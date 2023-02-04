09cfb34365331f2e9aac50593237a404e5f2435c
Chandra Panta C <chandra.panta345@hotmail.com>
2023-02-04 11:10:30 -0500

    Updates packages. Only starts app if no issues loading loaders.

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,25 @@ import express from "express";
 import Logger from "./loaders/logger";
 import http from "http";
 
-async function startServer() {
+function startServer() {
   const app = express();
   const server = http.createServer(app);
 
-  await require("./loaders").default(app, server);
-
-  server
-    .listen(config.port, () => {
-      Logger.info(`Server running on port ${config.port}`);
+  require("./loaders")
+    .default(app, server)
+    .then(() => {
+      server
+        .listen(config.port, () => {
+          Logger.info(`Server running on port ${config.port}`);
+        })
+        .on("error", (err) => {
+          Logger.error(`Server failed to start ${config.port}`);
+          Logger.error(err.message);
+          process.exit(1);
+        });
     })
-    .on("error", (err) => {
+    .catch(() => {
       Logger.error(`Server failed to start ${config.port}`);
-      Logger.error(err.message);
       process.exit(1);
     });
 }
